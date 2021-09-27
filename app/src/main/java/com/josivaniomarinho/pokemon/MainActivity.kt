5e8c64481af9cf5342fe5b222894cdc042acfae2
JosivanioMarinho <josivaniom69@gmail.com>
2021-09-27 10:28:55 -0300

    Projeto criado

---
+++ app/src/main/java/com/josivaniomarinho/pokemon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.josivaniomarinho.pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
